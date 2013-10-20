748446fc6857a628b421bb8005770c5c66639df0
noorus <noorus@localhost>
2013-10-20 06:19:48 +0300

    Sculpting some headers

--- include/nil.h
+++ include/nil.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#ifndef NTDDI_VERSION
+# define NTDDI_VERSION NTDDI_VISTA
+# define _WIN32_WINNT _WIN32_WINNT_VISTA
+#endif
+#include <sdkddkver.h>
+#include <windows.h>
+#include <dbt.h>
+#include <devguid.h>
+#include <hidclass.h>
+
+#include <stdlib.h>
+
+#include <string>
+#include <exception>
+
+namespace nil {
+
+  // stubb
+
+}
---
+++ include/nilUtil.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <windows.h>
+#include <devguid.h>
+
+namespace nil {
+
+# ifndef SAFE_DELETE
+#   define SAFE_DELETE(p) {if(p){delete p;(p)=NULL;}}
+# endif
+
+# ifndef SAFE_RELEASE
+#   define SAFE_RELEASE(p) {if(p){p->Release();(p)=NULL;}}
+# endif
+
+  static GUID g_HIDInterfaceGUID = { 0x4D1E55B2L, 0xF16F, 0x11CF, { 0x88, 0xCB, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30 } };
+
+}
