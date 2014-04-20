d77383f01071ffda732d1539c03e365ba268a953
noorus <noorus@users.noreply.github.com>
2014-04-21 00:09:47 +0300

    nilException.h documentation cleanup

--- include/nilException.h
+++ include/nilException.h
@@ -6,22 +6,20 @@ namespace Nil {
   //! \addtogroup Nil
   //! @{
 
-  //! \struct WinAPIError
-  //! \brief Container for a Windows API error description.
   struct WinAPIError
   {
     public:
-      uint32_t code; //!< WinAPI error code
-      String description; //!< WinAPI error description
+      uint32_t code;
+      String description;
   };
 
   //! \class Exception
-  //! \brief Main Nil exception class.
+  //! Main Nil exception class.
   //! \sa std::exception
   class Exception: public std::exception
   {
     public:
-      //! \brief Values that represent Type.
+      //! Type of exception.
       enum Type: int
       {
         Generic = 0,  //!< An enum constant representing the generic option
@@ -29,46 +27,33 @@ namespace Nil {
         DirectInput //!< DirectInput-specific error
       };
     private:
-      //! \brief Default constructor.
       Exception();
-    protected:
+
       Type mType; //!< Exception type
       String mDescription; //!< Exception description
       String mSource; //!< Exception source
       mutable String mFullDescription; //!< Full, extended description
       mutable utf8String mUTF8Description; //!< Full, extended description in UTF-8
-      variant<WinAPIError> mAdditional; //!< Internal additional exception data
+      variant<WinAPIError> mAdditional; //!< \b Internal Additional exception data
 
-      //! \brief Handles additional exception data for WinAPI/DI exceptions.
-      //! \param  hr HRESULT error code.
+      //! \b Internal Handle additional exception data for WinAPI/DI exceptions.
       void handleAdditional( HRESULT hr = 0 );
     public:
-      //! \brief Constructor.
-      //! \param  description The description.
-      //! \param  type        (Optional) the type.
+      //! Generic constructor.
       Exception( const String& description, Type type = Generic );
 
-      //! \brief Constructor.
-      //! \param  description The description.
-      //! \param  source      Source of exception.
-      //! \param  type        (Optional) the type.
+      //! Constructor with source.
       Exception( const String& description, const String& source,
         Type type = Generic );
 
-      //! \brief Constructor.
-      //! \param  description The description.
-      //! \param  source      Source of exception.
-      //! \param  hr          Additional HRESULT for WinAPI/DI.
-      //! \param  type        (Optional) the type.
+      //! Constructor with source and a WinAPI/DirectInput error code.
       Exception( const String& description, const String& source,
         HRESULT hr, Type type = Generic );
 
-      //! \brief Gets full, extended description of the exception.
-      //! \return The full description.
+      //! Get the full, extended description of the exception.
       virtual const String& getFullDescription() const;
 
-      //! \brief Gets the what.
-      //! \return Compact description.
+      //! Get the std::exception compatible exception description.
       virtual const char* what() const throw();
   };
 
